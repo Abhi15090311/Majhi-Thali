2f1550c7608366e7d0b0e1960cfca73d7aa9e6fc
Aneesh Bhattacharjee <aneeshb35@gmail.com>
2023-02-01 14:00:19 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+AOS.init()
+
+const add = document.getElementById('add-menu')
+const plus = document.getElementById('plus')
+const minus = document.getElementById('minus')
+let a = 0
+
+add.addEventListener('click', ()=>{
+    // add.innerHTML = `${a+=1}`
+    // plus.classList.add('active')
+    // minus.classList.add('active')
+    // add.innerHTML = `${minus}`
+
+
+})
